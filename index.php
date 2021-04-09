ae6100b6f58a44b3f85f63d170cd559a8e7920d4
Ellie Brayton <ebrayton@ufl.edu>
2021-04-08 19:24:00 -0500

    Adjusted color palette and heading sizes

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php get_header(); ?>
 
-<p class="text-primary">This is a sentence</p>
-<p class="text-seconday">This is a sentence</p>
-<p class="text-tertiary">This is a sentence</p>
-<p class="text-neutral">This is a sentence</p>
-<p class="text-accent">This is a sentence</p>
 
 <?php get_footer(); ?>
